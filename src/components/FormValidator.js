1f2ee46c138b5cc14e2567b1e8562625366c586d
OlgaSivyuk <olchik0308@yandex.ru>
2022-03-28 19:07:26 +0300

    del commnts

--- src/components/FormValidator.js
+++ src/components/FormValidator.js
@@ -81,14 +81,3 @@ export class FormValidator{
     });
   };
 };
-
-// const config = {
-//   formSelector: '.popup__form',
-//   inputSelector: '.popup__input',
-//   submitButtonSelector: '.popup__submit',
-//   inactiveButtonClass: 'popup__submit_disabled',
-//   inputErrorClass: 'popup__input_type_error',
-//   errorClass: 'popup__input-error_active'
-//   };
-
-//export { FormValidator, config }
